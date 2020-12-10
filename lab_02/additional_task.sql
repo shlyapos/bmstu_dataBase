3384b63e8e02ffab2adab26407f9b4143b0fec0d
Alexander <alexandrsuchkov67@gmail.com>
2020-12-10 23:02:12 +0300

    FIX: additional task

--- lab_02/additional_task.sql
+++ lab_02/additional_task.sql
@@ -25,15 +25,20 @@ INSERT INTO table2 (id, var2, date_from, date_to) VALUES(1, 'B', '2018-09-19', '
 
 
 SELECT table1.id, var1, var2,
-	CASE WHEN table1.date_from < table2.date_from
-		THEN table2.date_from
-		ELSE table1.date_from
+	CASE WHEN table1.date_from > table2.date_from OR table2.date_from IS NULL 
+		THEN table1.date_from 
+		ELSE table2.date_from
 	END AS date_from,
 	CASE WHEN table1.date_to < table2.date_to
 		THEN table1.date_to
 		ELSE table2.date_to
 	END AS date_to
 FROM table1 
-JOIN table2 ON table1.id = table2.id AND
-table1.date_from < table2.date_to AND
-table1.date_to > table2.date_from;
+JOIN table2 
+ON table1.id = table2.id AND
+(
+	table2.date_from BETWEEN table1.date_from AND table1.date_to
+	OR
+	table1.date_from BETWEEN table2.date_from AND table2.date_to
+)
+ORDER BY table1.date_from;
